2d9f91b9df53936cf4045fd2d6e13da4fc3f528d
uffemcev <96709114+uffemcev@users.noreply.github.com>
2026-01-27 21:12:15 +0700

    Add files via upload

---
+++ marzneshin.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#ЗАПРОС ДОМЕНА
+clear
+read -ep "Enter your domain:"$'\n' input_domain
+apt update && apt install sqlite3 -y
+read() { true; }
+
+# УСТАНОВКИ ПАНЕЛИ
+install_mode="1"
+input_domain=$input_domain
+configure_ssh_input="n"
+input_admin_user=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16; echo)
+enable_xhttp="y"
+enable_hysteria="y"
+configure_ufw_input="y"
+bash <(wget -qO- https://raw.githubusercontent.com/artemscine/xray-vps-setup/refs/heads/main/vps-setup.sh)
+
+# СЕРВИСЫ
+db_path="/opt/marzneshin-vps-setup/marzneshin_data/db.sqlite3"
+sqlite3 "$db_path" "INSERT INTO services (name) VALUES ('Service');"
+sqlite3 "$db_path" "INSERT INTO inbounds_services (inbound_id, service_id) VALUES (1, 1), (2, 1), (3, 1);"
+sqlite3 "$db_path" "UPDATE hosts SET remark='{TRANSPORT}' WHERE remark IS NOT NULL AND remark != '';"
+
+# НАСТРОЙКА РОУТИНГА
+subscription_url="https://github.com/hydraponique/roscomvpn-happ-routing/raw/main/Auto-routing%20for%20some%20panels/Marzneshin%20NON-JSON/subscription.py"
+subscription_file="/opt/marzneshin-vps-setup/marzneshin_data/templates/subscription.py"
+docker_compose_file="/opt/marzneshin-vps-setup/docker-compose.yml"
+mkdir -p /opt/marzneshin-vps-setup/marzneshin_data/templates
+curl -L "$subscription_url" -o "$subscription_file"
+sed -i '/marzneshin:/,/volumes:/s|volumes:|volumes:\n      - ./marzneshin_data/templates/subscription.py:/app/app/routes/subscription.py|' "$docker_compose_file"
+
+# ВЫДАЧА ДАННЫХ
+docker compose -f /opt/marzneshin-vps-setup/docker-compose.yml down && docker compose -f /opt/marzneshin-vps-setup/docker-compose.yml up -d
+clear
+echo "Panel: https://${VLESS_DOMAIN}/${MARZBAN_PATH}"
+echo "User: xray_admin"
+echo "Password: ${MARZBAN_PASS}"
