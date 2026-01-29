f1962bb8a7fff507ebf3cd0dc73b227268b95335
uffemcev <96709114+uffemcev@users.noreply.github.com>
2026-01-30 06:45:24 +0700

    Fix comments formatting in marzneshin.sh

--- marzneshin.sh
+++ marzneshin.sh
@@ -6,7 +6,7 @@ read -ep "Enter your domain:"$'\n' input_domain
 apt update && apt install sqlite3 -y
 read() { true; }
 
-# УСТАНОВКИ ПАНЕЛИ
+#УСТАНОВКА ПАНЕЛИ
 install_mode="1"
 input_domain=$input_domain
 configure_ssh_input="n"
@@ -16,14 +16,14 @@ enable_hysteria="y"
 configure_ufw_input="y"
 source <(wget -qO- https://github.com/artemscine/xray-vps-setup/raw/main/vps-setup.sh)
 
-# НАСТРОЙКИ БД
+#НАСТРОЙКА БД
 db_path="/opt/marzneshin-vps-setup/marzneshin_data/db.sqlite3"
 sqlite3 "$db_path" "INSERT INTO services (name) VALUES ('Service');"
 sqlite3 "$db_path" "INSERT INTO inbounds_services (inbound_id, service_id) SELECT id, (SELECT id FROM services WHERE name = 'Service') FROM inbounds;"
 sqlite3 "$db_path" "UPDATE hosts SET remark = (SELECT tag FROM inbounds WHERE inbounds.id = hosts.inbound_id) WHERE inbound_id IS NOT NULL;"
 sqlite3 "$db_path" "UPDATE settings SET subscription = json_set(subscription, '\$.profile_title', 'VPN', '\$.support_link', '');"
 
-#НАСТРОЙКИ ФРОНТА
+#НАСТРОЙКА ФРОНТА
 index_file="/opt/marzneshin-vps-setup/marzneshin_data/templates/index.html"
 script_file="/opt/marzneshin-vps-setup/marzneshin_data/templates/script.txt"
 docker_compose_file="/opt/marzneshin-vps-setup/docker-compose.yml"
@@ -44,7 +44,7 @@ sed -i "/<body>/r $script_file" "$index_file"
 sed -i '/marzneshin:/,/volumes:/s|volumes:|volumes:\n      - ./marzneshin_data/templates/index.html:/app/dashboard/dist/index.html|' "$docker_compose_file"
 rm "$script_file"
 
-# НАСТРОЙКА РОУТИНГА
+#НАСТРОЙКА РОУТИНГА
 subscription_url="https://github.com/hydraponique/roscomvpn-happ-routing/raw/main/Auto-routing%20for%20some%20panels/Marzneshin%20NON-JSON/subscription.py"
 subscription_file="/opt/marzneshin-vps-setup/marzneshin_data/templates/subscription.py"
 docker_compose_file="/opt/marzneshin-vps-setup/docker-compose.yml"
@@ -52,7 +52,7 @@ mkdir -p /opt/marzneshin-vps-setup/marzneshin_data/templates
 curl -L "$subscription_url" -o "$subscription_file"
 sed -i '/marzneshin:/,/volumes:/s|volumes:|volumes:\n      - ./marzneshin_data/templates/subscription.py:/app/app/routes/subscription.py|' "$docker_compose_file"
 
-# ВЫДАЧА ДАННЫХ
+#ВЫДАЧА ДАННЫХ
 docker compose -f /opt/marzneshin-vps-setup/docker-compose.yml down && docker compose -f /opt/marzneshin-vps-setup/docker-compose.yml up -d
 clear
 echo "Dashboard: https://${VLESS_DOMAIN}/${DASHBOARD_PATH}/"
