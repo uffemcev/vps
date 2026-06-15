e5c2db67670c842302397392c1069aac5f180f59
uffemcev <96709114+uffemcev@users.noreply.github.com>
2026-06-15 23:38:58 +0700

    Update bridge2.sh

--- bridge2.sh
+++ bridge2.sh
@@ -7,46 +7,34 @@ read -ep "Enter your EN domain:"$'\n' en_domain
 read -ep "Enter your EN ip:"$'\n' en_ip
 read -ep "Enter your EN login:"$'\n' en_login
 read -ep "Enter your EN password:"$'\n' en_password
-apt update && apt install -y sqlite3 sshpass
+apt update && apt install -y sshpass
 read() { true; }
 
 #НАСТРОЙКА EN
-en_url=$(sshpass -p "$en_password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$en_login@$en_ip" bash -s "$en_domain" << 'EOF'
+remote_output=$(sshpass -p "$en_password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$en_login@$en_ip" bash -s "$en_domain" << 'EOF'
     read() { true; }
     export TERM=xterm
-    export install_choice=1
-    export input_domain="$1"
-    export configure_ssh_input="n"
-    export configure_warp_input="n"
-    source <(wget -qO- https://github.com/Akiyamov/xray-vps-setup/raw/main/vps-setup.sh) > /dev/null 2>&1
-    echo "vless://$XRAY_UUID@$VLESS_DOMAIN:443?type=tcp&security=reality&pbk=$XRAY_PBK&fp=chrome&sni=$VLESS_DOMAIN&sid=&spx=%2F&flow=xtls-rprx-vision#EN"
+    export choice_warp="n"
+    export choice_mtp="n"
+    export fp_choice="2"
+    source <(wget -qO- https://raw.githubusercontent.com/xVRVx/autoXRAY/main/autoXRAY1.sh) "$en_domain" > /dev/null 2>&1
+    echo "$linkRTY2"
+    echo "$configListLink"
 EOF
 )
+mapfile -t en_url <<< "$remote_output"
 
 #НАСТРОЙКА RU
-export install_choice=2
-export input_domain=$ru_domain
-export configure_ssh_input="n"
-export configure_warp_input="n"
-source <(wget -qO- https://github.com/Akiyamov/xray-vps-setup/raw/main/vps-setup.sh)
+export choice_mtp="n"
+export fp_choice="5"
+source <(wget -qO- https://raw.githubusercontent.com/xVRVx/autoXRAY/main/autoXRAYselfRUbrEUxhttp.sh) "$ru_domain" "${en_url[0]}"
 
 #УСТАНОВКА ZAPRET
 while sleep 1; do echo; done | script -q -e -c "wget https://raw.githubusercontent.com/IndeecFOX/z4r/4/z4r && sh z4r" /dev/null
 
-#УСТАНОВКА БАЗ
-export geoip_url="https://cdn.jsdelivr.net/gh/hydraponique/roscomvpn-geoip/release/geoip.dat"
-export geosite_url="https://cdn.jsdelivr.net/gh/hydraponique/roscomvpn-geosite/release/geosite.dat"
-export geoip_file="/opt/xray-vps-setup/xray-core/geoip.dat"
-export geosite_file="/opt/xray-vps-setup/xray-core/geosite.dat"
-export docker_compose_file="/opt/xray-vps-setup/docker-compose.yml"
-curl -L "$geoip_url" -o "$geoip_file"
-curl -L "$geosite_url" -o "$geosite_file"
-sed -i '/marzban:/,/volumes:/s|volumes:|volumes:\n      - ./xray-core/geosite.dat:/usr/local/share/xray/geosite.dat|' "$docker_compose_file"
-sed -i '/marzban:/,/volumes:/s|volumes:|volumes:\n      - ./xray-core/geoip.dat:/usr/local/share/xray/geoip.dat|' "$docker_compose_file"
+#РЕДАКТИРОВАНИЕ МАРШРУТОВ
 
 #ВЫДАЧА ДАННЫХ
-docker compose -f /opt/xray-vps-setup/docker-compose.yml down && docker compose -f /opt/xray-vps-setup/docker-compose.yml up -d
+systemctl restart xray
 clear
-echo "Dashboard: https://${VLESS_DOMAIN}/${MARZBAN_PATH}"
-echo "User: ${MARZBAN_USER}"
-echo "Password: ${MARZBAN_PASS}"
+echo "EN подписка: ${en_url[1]}"
